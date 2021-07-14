a6bef8f5aed94847b66fd290a7bc9de3ff4beeeb
Shruti Kashyap <71397281+shrutikdev@users.noreply.github.com>
2021-07-14 12:42:30 +0530

    Create pattern1.cpp

---
+++ pattern1.cpp
@@ -0,0 +1,27 @@
+//Star pattern, if given row=4 and col=5
+//*****
+//*****
+//*****
+//*****
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int row,col;
+    cin>>row>>col;
+
+
+    for(int i =1;i<=row;i++)
+    {
+        for(int j =1;j<=col;j++)
+        {
+                cout<<"*";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+
+
+}
